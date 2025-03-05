3caa125b7ece09fbb63992865f70264618cf64e3
LaiYC0204 <s1116008@mail.yzu.edu.tw>
2025-03-05 15:50:13 +0800

    <FEAT>新增 Header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import Script from "next/script";
+import "../styles/globals.css"
+import { Header } from "../components";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,8 +26,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" />
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <Header />
         {children}
+        <Script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+          strategy="beforeInteractive"
+        />
       </body>
     </html>
   );
